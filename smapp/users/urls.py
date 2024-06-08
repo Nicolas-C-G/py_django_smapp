aa525c976df73ddbf93bfa03a787aabd2e387fab
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-06-08 16:57:31 -0400

    password change functionalities: done

--- smapp/users/urls.py
+++ smapp/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_view
 
 
 app_name = 'users'
@@ -7,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('password_change/',auth_view.PasswordChangeView.as_view(template_name='users/password_change_form.html', success_url='done'), name='password_change'),
+    path('password_change/done/', auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='done'),
 ]
