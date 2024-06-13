aae8c3e5b2601995c38b484474e8741e5ca7a83f
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2024-06-13 17:34:21 -0400

    Post view: done

--- smapp/posts/views.py
+++ smapp/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .forms import PostCreateForm
 # Create your views here.
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
